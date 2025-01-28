a1251ebcf6d12adfcb04c8fe0fddb00b7dab7f99
STEFANOVITCH-Ilann-24019037 <ilann.stefanovitch@etu.univ-amu.fr>
2025-01-28 15:16:46 +0100

    test

---
+++ src/Circle.java
@@ -0,0 +1,28 @@
+
+
+public class Circle {
+	
+    private double radius ;
+    private String color;
+    
+    public Circle() {
+        radius = 1.0;
+        color = "red";
+    }
+    public Circle(double r) {
+        radius = r;
+        color  = "red";
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+    public double getCircumference() {
+        return 2 * Math.PI * radius;
+    }
+}
--- src/fr/IlannStefanovitch/Ex1/Exo1.java
+++
@@ -1,6 +0,0 @@
-package fr.IlannStefanovitch.Ex1;
-
-public class Exo1 {
-	
-	public static void main (String []args)
-}
