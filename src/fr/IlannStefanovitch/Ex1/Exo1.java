c82cfe4943701c5e0dff3bf46d5a561af7e5bc69
STEFANOVITCH-Ilann-24019037 <ilann.stefanovitch@etu.univ-amu.fr>
2025-01-28 14:52:19 +0100

    Exo1

---
+++ src/fr/IlannStefanovitch/Ex1/Exo1.java
@@ -0,0 +1,6 @@
+package fr.IlannStefanovitch.Ex1;
+
+public class Exo1 {
+	
+	public static void main (String []args)
+}
--- src/helloW.java
+++ src/helloW.java
@@ -1,7 +1,7 @@
 
 public class helloW {
-	public static void main (String [] args ) {
-		System.out.println("hello world c'est ilann ");
-	}
 
+	public static void main (String [] args) {
+		System.out.println("hello world de Ilann ");
+	}
 }
